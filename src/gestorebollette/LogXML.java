dad53d9c98920131611973b22c039a54cda69be9
Edoardo Spigarelli <edospi20@hotmail.it>
2015-09-11 16:48:27 +0200

    aaaaa

---
+++ src/gestorebollette/LogXML.java
@@ -0,0 +1,14 @@
+
+package gestorebollette;
+
+public class LogXML {
+    private String ip;
+    private String data;
+    private String ora;
+    private String evento;
+    
+    LogXML(String[] log){
+        ip = log[0];
+        data = 
+    }
+}
